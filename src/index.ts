0fdbef7776dac94f98624e9a2b9a013543a145b9
Nicolas JUELLE <n.juelle@gmail.com>
2022-05-21 15:30:42 +0200

    chore: add TypeScript

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,20 @@ const discountOffers = [
   new DiscountOffer("Velib", 20, 30),
   new DiscountOffer("Naturalia", 10, 5),
   new DiscountOffer("Vinted", 5, 40),
-  new DiscountOffer("Ilek", 15, 40)
+  new DiscountOffer("Ilek", 15, 40),
 ];
 const store = new Store(discountOffers);
 
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(store.updateDiscounts()));
+  log.push(store.updateDiscounts());
 }
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+fs.writeFile("output.txt", JSON.stringify(log), (err) => {
   if (err) {
-    console.log("error");
+    console.error(err);
   } else {
     console.log("success");
   }
